4156d74d8374de4429f6571b84fe43299d2a8592
Jackieto <jackietoyt@gmail.com>
2025-04-01 11:57:30 +0200

    done exercises 5 and 6

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(x,y) {
+    if(x < 0 || y < 0){
+        return "ERROR";
+    }
 
+    if(Number.isInteger(x) == false || Number.isInteger(y) == false){
+        return "ERROR";
+    }
+
+    let sum = 0;
+
+    for (let index = Math.min(x,y); index <= Math.max(x,y); index++) {
+        sum += index;        
+    }
+
+return sum;
 };
+
+ // returns the sum of 1 + 2 + 3 + 4 which is 10
+console.log(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
