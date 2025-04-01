5f2a19108464591fafca2f4a6af84d24aa1599bc
Jackieto <jackietoyt@gmail.com>
2025-04-01 12:57:57 +0200

    Done exercises 7 and 8

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,31 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+
+  // 	x_C = (x − 32) × ⁠5/9⁠ °C 
+  let res = (temp - 32) * (5/9);
+  let rounded_res = (Math.round(res * 10) / 10);
+  return rounded_res 
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+
+  // 	x_f = (x × ⁠9/5⁠ + 32) °F 
+  let res = 0;
+  if(temp < 0){
+    res = (1.8 * temp) + 32;
+  }else{
+     res = (temp  * (9/5) + 32 );
+  }
+
+  let rounded_res = (Math.round(res * 10) / 10);
+
+
+  return rounded_res
+
 };
+
+convertToCelsius(100);
 
 // Do not edit below this line
 module.exports = {
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,63 @@
-const add = function() {
-	
-};
+const add = function(x,y) {
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+  return x + y;
 
 };
 
-const power = function() {
-	
+const subtract = function(x,y) {
+	return x - y;
 };
 
-const factorial = function() {
+const sum = function(arr) {
 	
+  if(arr == undefined || arr.length == 0){
+      return 0;
+  }
+
+  if(arr.length == 1){
+    return arr[0];
+  }
+
+  if(arr.length == 2){
+    return arr[0] + arr[1];
+  }else{
+
+    let sum = 0;
+    for (let index = 0; index < arr.length; index++) {
+      sum += arr[index];
+    }
+    return sum;
+
+  }
+
+};
+
+const multiply = function(...args) {
+  // If the argument is an array, flatten it into individual numbers
+  if (Array.isArray(args[0])) {
+    args = args[0];
+  }
+
+  let total = 1;
+  
+  for (let index = 0; index < args.length; index++) {
+    total *= args[index];
+  }
+
+  return total;
+};
+
+const power = function(x,y) {
+	return x**y;
+};
+
+const factorial = function(number) {
+  total = 1
+  while (number > 0) {
+    total *= number
+    number = number - 1
+  }
+  return total
 };
 
 // Do not edit below this line
